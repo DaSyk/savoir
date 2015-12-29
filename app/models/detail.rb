312da87fe0f2d2d719463bdb34e1b27e9ec02f58
Jascha Schmittwilken <jascha.schmittwilken@gmx.de>
2015-12-29 18:30:28 +0100

    29.12.2015

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,5 +1,5 @@
 class Detail < ActiveRecord::Base
   has_many :suits, :dependent => :destroy
   has_many :facilities, :dependent => :destroy
-  accepts_nested_attributes_for :suits, :facilities
+  accepts_nested_attributes_for :suits, :facilities, :allow_destroy => true
 end
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,5 +1,5 @@
 class Facility < ActiveRecord::Base
+  belongs_to :detail
 	has_and_belongs_to_many :houses
-
   accepts_nested_attributes_for :houses
 end
